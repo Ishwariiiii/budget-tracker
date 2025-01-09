b70844068c2a34e6c77e29a0530a790df637fcb7
Ishwariiiii <ishwarikanathe@gmail.com>
2025-01-09 14:32:03 +0530

    toastify

--- src/Components/Remaining.jsx
+++ src/Components/Remaining.jsx
@@ -7,21 +7,16 @@ import 'react-toastify/dist/ReactToastify.css';
 const Remaining = () => {
     const { expenses, budget } = useContext(AppContext);
 
-    // Calculate total expenses
     const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
 
-    // Calculate remaining balance
     const remaining = budget - totalExpenses;
 
-    // Determine the alert type based on remaining balance
     const alertType = remaining < 0 ? 'alert-danger' : 'alert-success';
 
-    // Show toast alert if remaining balance goes negative
     useEffect(() => {
         if (remaining < 0) {
             toast.error("You have exceeded your budget!", {
-                // position: toast.POSITION.TOP_RIGHT,
-                autoClose: 5000,
+                autoClose: 1000,
             });
         }
     }, [remaining]);
